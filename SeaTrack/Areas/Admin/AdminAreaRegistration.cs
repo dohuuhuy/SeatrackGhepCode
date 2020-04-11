653f1fcc40ace2456fb3add8daaa56fb2f1e1a32
mrhuy3200 <mrhuy3200113@gmail.com>
2020-04-11 15:53:54 +0700

    r

--- SeaTrack/Areas/Admin/AdminAreaRegistration.cs
+++ SeaTrack/Areas/Admin/AdminAreaRegistration.cs
@@ -14,6 +14,7 @@ namespace SeaTrack.Areas.Admin
 
         public override void RegisterArea(AreaRegistrationContext context) 
         {
+
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
