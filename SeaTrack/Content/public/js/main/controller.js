1cbd17500233be426ddf5cdd7881f9f3490ebcf2
huy <54833364+dohuuhuy@users.noreply.github.com>
2020-04-22 16:46:39 +0700

    all

--- SeaTrack/Content/public/js/main/controller.js
+++ SeaTrack/Content/public/js/main/controller.js
@@ -1,4 +1,32 @@
-﻿function GetInfo_User() {
+﻿function exportTableToExcel(tableID, filename = '') {
+    var downloadLink;
+  
+    var dataType = '  application / vnd.openxmlformats - officedocument.spreadsheetml.sheet';
+    var tableSelect = document.getElementById(tableID);
+    var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
+
+
+    filename = filename ? filename + '.xlsx' : 'excel_data.xlsx';
+
+    downloadLink = document.createElement("a");
+
+    document.body.appendChild(downloadLink);
+
+    if (navigator.msSaveOrOpenBlob) {
+        var blob = new Blob(['\ufeff', tableHTML], {
+            type: dataType
+        });
+        navigator.msSaveOrOpenBlob(blob, filename);
+    } else {
+        downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+
+        downloadLink.download = filename;
+
+        downloadLink.click();
+    }
+}
+
+function GetInfo_User() {
 
     $.ajax({
         type: 'GET',
